efb3675d2f2ac97234a1060ebcd63f1294e96be1
mykhailo <michaelh12r2@gmail.com>
2022-03-11 13:52:31 +0200

    validation

---
+++ Notes.Backend/Notes.Application/Notes/Commands/CreateNote/CreateNoteCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Notes.Application.Notes.Commands.CreateNote
+{
+    internal class CreateNoteCommandValidator : AbstractValidator<CreateNoteCommand>
+    {
+        public CreateNoteCommandValidator()
+        {
+            RuleFor(createNoteCommand =>
+                createNoteCommand.Title).NotEmpty().MaximumLength(250);
+            RuleFor(createNoteCommand =>
+                createNoteCommand.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
